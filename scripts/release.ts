fb22c8e70902422c2a4a793692db4aad79a61c73
wangkaiwd <1162486176@qq.com>
2022-10-30 17:42:33 +0800

    chore(release): start rewrite release scripts

--- scripts/release.ts
+++ scripts/release.ts
@@ -1,1 +1,37 @@
-console.log("release");
+import prompts, { PromptObject } from "prompts";
+
+import { inc, ReleaseType } from "semver";
+import { bumpPkgsVersion, getPkgsInfo, step } from "./utils";
+
+const { version: targetVersion } = getPkgsInfo();
+
+const increments: ReleaseType[] = ["major", "minor", "patch"];
+
+const main = async () => {
+  const question: PromptObject = {
+    type: "select",
+    name: "releaseType",
+    message: "Pick a new version",
+    choices: increments.map((item) => ({
+      title: item,
+      value: item,
+      description: inc(targetVersion, item)!,
+    })),
+  };
+  const { releaseType } = await prompts(question);
+  const incVersion = inc(targetVersion, releaseType)!;
+  const { confirm } = await prompts({
+    type: "confirm",
+    name: "confirm",
+    message: `New version is ${incVersion}, continue ?`,
+    initial: true,
+  });
+  if (confirm) {
+    step("bump packages version ...");
+    await bumpPkgsVersion(incVersion);
+  }
+};
+
+main().catch((err) => {
+  console.error(err);
+});
