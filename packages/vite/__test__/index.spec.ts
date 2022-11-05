bd5e5eb079b2c6de82f6d56846d778af3c38fe82
wangkaiwd <1162486176@qq.com>
2022-11-05 17:22:29 +0800

    feat: create static server to host assets

--- packages/vite/__test__/index.spec.ts
+++ packages/vite/__test__/index.spec.ts
@@ -1,9 +1,8 @@
 import { describe, it, expect } from "vitest";
-import { add } from "../src";
 
 describe("add", () => {
   it("should add two numbers", () => {
-    const sum = add(1, 2);
+    const sum = 1 + 2;
     expect(sum).toBe(3);
   });
 });
--- packages/vite/src/server.ts
+++ packages/vite/src/server.ts
@@ -1,18 +1,24 @@
 import http from "node:http";
 import chalk from "chalk";
+import path from "node:path";
+import mime from "mime-types";
+import fs from "node:fs/promises";
+import process from "node:process";
 import pkg from "../package.json";
 
 const defaultPort = 5173;
 
 export interface ServerOptions {
   host?: boolean;
-  port?: string | number;
+  port?: number;
 }
 
 const defaultOptions: Required<ServerOptions> = {
   port: 5173,
   host: false,
 };
+
+// fixme: 1. read large file ?  2. cache resource
 
 export class Server {
   private options: Required<ServerOptions>;
@@ -21,20 +27,45 @@ export class Server {
     this.options = Object.assign(defaultOptions, options);
   }
 
-  createServer() {
-    const server = http.createServer((req, res) => {
-      res.writeHead(200, { "Content-Type": "application/json" });
-      res.end(
-        JSON.stringify({
-          data: "Hello World!",
-        })
-      );
+  createServer = () => {
+    const server = http.createServer(async (req, res) => {
+      const { url } = req;
+      const formattedUrl = url === "/" ? "/index.html" : url;
+
+      function notFoundHandler() {
+        res.setHeader("Content-Type", "text/html; charset=utf-8");
+        res.end("404 Not Found");
+      }
+
+      if (!formattedUrl) {
+        notFoundHandler();
+        return;
+      }
+      const extension = path.extname(formattedUrl);
+      const mimeType = mime.contentType(extension);
+      if (!mimeType) {
+        notFoundHandler();
+        return;
+      }
+      let source;
+      try {
+        source = await fs.readFile(path.join(process.cwd(), formattedUrl));
+        res.setHeader("Content-Type", mimeType);
+        res.end(source);
+      } catch (e) {
+        notFoundHandler();
+      }
     });
 
     server.listen(this.options.port ?? defaultPort, () => {
-      console.log(`${chalk.green(`SVITE v${pkg.version}`)}  ready for `);
+      const interval = Date.now() - global.start;
+      console.log(
+        `${chalk.green(`SVITE v${pkg.version}`)}  ready for ${chalk.cyan(
+          `${interval}ms`
+        )}`
+      );
       console.log(`-> Local:   http://localhost:${this.options.port}`);
     });
     return server;
-  }
+  };
 }
