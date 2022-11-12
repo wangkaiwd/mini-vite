b3650e9cfa7f22cce292635ad6f987901fac87fa
wangkaiwd <1162486176@qq.com>
2022-11-12 19:28:18 +0800

    feat: refactor class to function

--- packages/vite/src/index.ts
+++ packages/vite/src/index.ts
@@ -1,5 +1,5 @@
 import { cac } from "cac";
-import { Server, ServerOptions } from "./server";
+import createServer, { ServerOptions } from "./createServer";
 
 const cli = cac("svite");
 
@@ -9,9 +9,9 @@ cli
   .command("[root]", "start dev server")
   .option("--host", "display host")
   .option("-p, --port [port]", "specify start server port")
-  .action((root: string, options: ServerOptions) => {
-    const server = new Server(options);
-    server.createServer();
+  .action(async (root: string, options: ServerOptions) => {
+    const server = await createServer(options);
+    console.log("server", server);
   });
 
 cli.help();
